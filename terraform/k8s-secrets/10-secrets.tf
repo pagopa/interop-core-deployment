fca0c3649f5212ae437d53868e192d0af83b7cbf
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-09-09 10:11:34 +0200

    feat: edit logic to synchronize Kubernetes secrets only to the namespace that matches the 'environment' input of the workflow  (#266)

--- terraform/k8s-secrets/10-secrets.tf
+++ terraform/k8s-secrets/10-secrets.tf
@@ -16,21 +16,19 @@ data "aws_secretsmanager_secret" "tagged_object" {
 data "aws_secretsmanager_secret_version" "filtered" {
   depends_on = [data.aws_secretsmanager_secret.tagged_object]
 
-  for_each = { for key, object in data.aws_secretsmanager_secret.tagged_object : key => object if(object.tags["EKSClusterName"] == var.eks_cluster_name && (contains(local.terraform_states_list, object.tags["TerraformState"]))) }
+  for_each = { for key, object in data.aws_secretsmanager_secret.tagged_object : key => object if(object.tags["EKSClusterName"] == var.eks_cluster_name && (contains(local.terraform_states_list, object.tags["TerraformState"])) && contains(split(" ", object.tags["EKSClusterNamespacesSpaceSeparated"]), var.env) ) }
 
   secret_id = each.value.name
 }
 
 locals {
-  sv_namespaces_pairs = flatten([
-    for sv_key, sv_value in data.aws_secretsmanager_secret_version.filtered : [
-      for ns in toset(split(" ", (data.aws_secretsmanager_secret.tagged_object[sv_key].tags["EKSClusterNamespacesSpaceSeparated"]))) : {
-        eks_replica_secret_name = data.aws_secretsmanager_secret.tagged_object[sv_value.secret_id].tags["EKSReplicaSecretName"],
-        secret_version          = sv_value,
-        namespace               = ns
-      }
-    ]
-  ])
+  sv_namespaces_pairs = [
+    for sv_key, sv_value in data.aws_secretsmanager_secret_version.filtered : {
+      eks_replica_secret_name = data.aws_secretsmanager_secret.tagged_object[sv_value.secret_id].tags["EKSReplicaSecretName"],
+      secret_version          = sv_value,
+      namespace               = var.env
+    }
+  ]
 }
 
 resource "time_static" "secret_string_update" {
